3731454ce4aa9f7e5e3b7227f2819d1f1537b15c
Robert Toth <robert.toth@thetatech.ai>
2022-09-02 23:42:27 +0000

    Output folder for histoqc created.

--- histoqc/__init__.py
+++ histoqc/__init__.py
@@ -1,7 +1,12 @@
 from girder import plugin
 from girder.api import access
-from girder.api.rest import boundHandler
+from girder.api.rest import boundHandler, getApiUrl
 from girder.api.describe import Description, autoDescribeRoute
+from girder.models.folder import Folder
+import requests
+
+
+histoqc_output_folder_name = 'histoqc-output-folder'
 
 
 class GirderPlugin(plugin.GirderPlugin):
@@ -9,7 +14,10 @@ class GirderPlugin(plugin.GirderPlugin):
     CLIENT_SOURCE_PATH = 'web_client'
 
     def load(self, info):
-        info['apiRoot'].folder.route('GET', (':id', 'histoqc-results'), getHistoQCResultsHandler)
+
+        apiRoot = info['apiRoot']
+        
+        apiRoot.folder.route('GET', (':id', 'histoqc-results'), getHistoQCResultsHandler)
         #info['apiRoot'].item.route('PUT', (':id', 'histoqc-upload'), uploadHandler)
         #info['apiRoot'].item.route('GET', (':id', 'thumbnail-rest'), restRequest)
 
@@ -20,7 +28,52 @@ class GirderPlugin(plugin.GirderPlugin):
     Description('Retrieve HistoQC results for every image in the folder')
 )
 def getHistoQCResultsHandler(self, id, params):
-    # get the images for that folder
     print(f"Getting histo qc results.")
+
+    print('Getting histoqc output folder...')
+    output_folder = getHistoqcOutputFolder(self)
+    print('output_folder = {output_folder}')
+
     return {"hello": "world"}
 
+
+
+def getHeaders(self):
+    return {
+        'Content-type': 'application/json',
+        'Accept': 'application/json',
+        'Girder-Token': self.getCurrentToken()['_id']
+    }
+
+
+# or make it if it does not yet exist
+def getHistoqcOutputFolder(self):
+    user = self.getCurrentUser()
+    print(f'user = {user}')
+
+    print(f'Searching for output folder {histoqc_output_folder_name}...')
+    found_folder = Folder().findOne({'name': histoqc_output_folder_name})
+
+    if not found_folder:
+        print('Folder not found. Creating it.')
+
+        url = getApiUrl() + '/folder'
+        print(f'url = {url}')
+
+        data = {
+            'parentType': "user",
+            'parentId': str(user['_id']),
+            'name': histoqc_output_folder_name,
+            'reuseExisting': True,
+            'public': False
+        }
+        print(f'data = {data}')
+
+        response = requests.post(url, params=data, headers=get_headers(self))
+        print(f'response = {response}')
+
+        found_folder = Folder().findOne({'name': histoqc_output_folder_name})
+
+    print(f'found_folder = {found_folder}')
+    return found_folder
+    
