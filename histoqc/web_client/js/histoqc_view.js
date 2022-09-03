1089b23c9396d0d3e4e50c8dcfe2444bcac56709
Robert Toth <robert.toth@thetatech.ai>
2022-09-03 07:32:01 +0000

    Log is displayed while async job runs.

--- histoqc/web_client/js/histoqc_view.js
+++ histoqc/web_client/js/histoqc_view.js
@@ -1,18 +1,26 @@
 import { getCurrentToken } from '@girder/core/auth';
 
+function getHeaders() {
+  return {
+    "Content-Type": "application/json; charset=utf-8",
+    'Girder-Token': getCurrentToken()
+  }
+}
+
+var histoqc_status_id = '#trigger-job-status'
+
+// the /api/v1 can be gotten from some girder javascript call
+function getApiRoot() { return window.location.origin + '/api/v1'}
+
 export function triggerHistoQCJob() {
 
-  const apiRoot = window.location.origin + '/api/v1'
-  console.log('apiRoot ', apiRoot)
+  const apiRoot = getApiRoot()
   const triggerJobUrl = apiRoot + '/folder/' + getFolderId() + '/histoqc'
   console.log('triggerJobUrl', triggerJobUrl)
   
   const postDetails = {
     method: 'post',
-    headers: {
-      "Content-Type": "application/json; charset=utf-8",
-      'Girder-Token': getCurrentToken()
-    },
+    headers: getHeaders(),
     mode: "cors"
   }
 
@@ -29,12 +37,46 @@ export function triggerHistoQCJob() {
 
       const jobId = myJson['_id']
       $('#trigger-job-button').hide()
-      $('#trigger-job-status').text('HistoQC job ' + jobId + ' has started. Please wait...')
-      $('#trigger-job-status').show()
+      $(histoqc_status_id).text('HistoQC job ' + jobId + ' has started. Please wait...')
+      $(histoqc_status_id).show()
+
+      watchJob(jobId)
 
     }).catch( err => {
       console.log( 'Fetch Error :-S', err );
     });
+}
+
+function watchJob(jobId) {
+  const checkSeconds = 2
+  setInterval(function(){
+    const url = getApiRoot() + '/job/' + jobId
+
+    const details = {
+      method: 'get',
+      headers: getHeaders(),
+      mode: "cors"
+    }
+
+    fetch(url, details)
+      .then( response => {
+        if ( response.status !== 200 ) {
+          console.log('Looks like there was a problem. Status Code: ' + response.status);
+          return;
+        }
+        console.log( response.headers.get("Content-Type"));
+        return response.json();
+      }).then( myJson => {
+        console.log(JSON.stringify(myJson));
+        const log = myJson['log']
+        let text = ''
+        for (const logLine of log) {
+          text = text + '<br/>' + logLine
+        }
+        $(histoqc_status_id).html(text)
+      });
+
+  }, checkSeconds * 1000);     
 }
 
 function getFolderId() {
@@ -57,9 +99,9 @@ export function renderHistoQC(callingThis, widget) {
   window.triggerHistoQCJob = triggerHistoQCJob
   const buttonHTML = `
     <button id="trigger-job-button" onclick="triggerHistoQCJob()">Click here to run HistoQC on all images in this folder.</button>
-    <p id="trigger-job-status">Job running.</p>
+    <p id="trigger-job-status">HistoQC status will go here.</p>
     <div id="parac-svg-container" />
   `
   widget.after(buttonHTML)
-  $('#trigger-job-status').hide()
+  $(histoqc_status_id).hide()
 }
