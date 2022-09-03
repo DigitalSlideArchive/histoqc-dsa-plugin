972354a92208ac13e86960208937c6a6b34fdfa4
Robert Toth <robert.toth@thetatech.ai>
2022-09-03 08:33:49 +0000

    Display a table with the histoqc results thumbnails.

--- histoqc/web_client/js/histoqc_view.js
+++ histoqc/web_client/js/histoqc_view.js
@@ -8,6 +8,7 @@ function getHeaders() {
 }
 
 var histoqc_status_id = '#trigger-job-status'
+var histoqc_table_id = '#histoqc-table-div'
 
 // the /api/v1 can be gotten from some girder javascript call
 function getApiRoot() { return window.location.origin + '/api/v1'}
@@ -39,6 +40,7 @@ export function triggerHistoQCJob() {
       $('#trigger-job-button').hide()
       $(histoqc_status_id).text('HistoQC job ' + jobId + ' has started. Please wait...')
       $(histoqc_status_id).show()
+      $(histoqc_table_id).hide()
 
       watchJob(jobId)
 
@@ -49,7 +51,7 @@ export function triggerHistoQCJob() {
 
 function watchJob(jobId) {
   const checkSeconds = 2
-  setInterval(function(){
+  var logTimer = setInterval(function(){
     const url = getApiRoot() + '/job/' + jobId
 
     const details = {
@@ -73,7 +75,17 @@ function watchJob(jobId) {
         for (const logLine of log) {
           text = text + '<br/>' + logLine
         }
-        $(histoqc_status_id).html(text)
+        if (log.length > 0) {
+          $(histoqc_status_id).html(text)
+        }
+
+        const status = myJson['status']
+        if (status > 2) {
+          clearInterval(logTimer);
+          $(histoqc_status_id).hide()
+          $(histoqc_table_id).show()
+          generateTable()
+        }
       });
 
   }, checkSeconds * 1000);     
@@ -98,10 +110,73 @@ export function renderHistoQC(callingThis, widget) {
 
   window.triggerHistoQCJob = triggerHistoQCJob
   const buttonHTML = `
-    <button id="trigger-job-button" onclick="triggerHistoQCJob()">Click here to run HistoQC on all images in this folder.</button>
+    <button id="trigger-job-button" onclick="triggerHistoQCJob()">Click here to (re)run HistoQC on all images in this folder.</button>
     <p id="trigger-job-status">HistoQC status will go here.</p>
+    <div id="histoqc-table-div" />
     <div id="parac-svg-container" />
   `
   widget.after(buttonHTML)
   $(histoqc_status_id).hide()
+  $('#histoqc-table-div').hide()
+  generateTable()
+}
+
+function generateTable() {
+  const url = getApiRoot() + '/folder/' + getFolderId() + '/histoqc'
+  const details = {
+    method: 'get',
+    headers: getHeaders(),
+    mode: "cors"
+  }
+  fetch(url, details)
+    .then( response => {
+      if ( response.status !== 200 ) {
+        console.log('Looks like there was a problem. Status Code: ' + response.status);
+        return;
+      }
+      console.log( response.headers.get("Content-Type"));
+      return response.json();
+    }).then( resp => {
+      let tableHTML = `
+        <h3>HistoQC Outputs</h3>
+        <p>Click on any image.</p>
+        <table>
+      `    
+      let row1;
+      for (const row of resp) {
+        if (row['histoqc_outputs'].length > 0) {
+          row1 = row;
+          break;
+        }
+      }
+      row1['histoqc_outputs'].sort((a, b) => a.histoqcType < b.histoqcType ? 1 : -1)
+      tableHTML += `<tr> <th> Source </th>`
+      row1['histoqc_outputs'].forEach(column => {
+        tableHTML += `<th> ${column.histoqcType} </th>`
+      })
+      tableHTML += `</tr>`
+      resp.forEach(row => {
+        if (row['histoqc_outputs'].length > 0) {
+            tableHTML += '<tr>'
+            tableHTML += `
+                    <td>
+                        ${row['source_image'].name}
+                    </td>
+            `
+          row['histoqc_outputs'].sort((a, b) => a.histoqcType < b.histoqcType ? 1 : -1)
+          row["histoqc_outputs"].forEach(column => {
+            tableHTML += `<td style="padding:5px;"> 
+                    <a href="/#item/${column._id}">
+                    <img src="/api/v1/item/${column._id}/tiles/thumbnail"/>
+                    </a>
+                </td>`
+            console.log("HERES THE DONE RESPONSE: ", resp)
+          })
+          tableHTML += '</tr>'
+        }
+      })
+      tableHTML += '</table>'
+      $(histoqc_table_id).html(tableHTML)
+      $(histoqc_table_id).show()
+    });
 }
