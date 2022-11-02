01dad45f68d6d33cbad2390de4cc96be89e16f98
Robert Toth <robert.toth@thetatech.ai>
2022-11-02 22:39:58 +0000

    Inputing the image item id into the cli worker

--- slicer_cli_plugin/HistoQC/HistoQC.py
+++ slicer_cli_plugin/HistoQC/HistoQC.py
@@ -3,9 +3,32 @@ import pprint
 from slicer_cli_web import ctk_cli_adjustment  # noqa - imported for side effects
 from ctk_cli import CLIArgumentParser
 
+import os
+
+import logging
+logging.basicConfig(level=logging.INFO)
+
+
+def get_item(args):
+    image_path = os.path.abspath(args.input_image)
+    logging.info(f'image_path = {image_path}')
+    logging.info(f'image exists = {os.path.isfile(image_path)}')
+
+    subdir = os.path.basename(os.path.dirname(image_path))
+    item_id = subdir
+    logging.info(f'item_id = {item_id}')
+    return {'image_path': image_path, 'item_id': item_id}
+
+
 def main(args):
-    print('Entering main function.')
-    print('%r' % args)
+    logging.info('Entering main function')
+    logging.info('args = %r' % args)
+
+    item = get_item(args)
+    logging.info(f'item = {item}')
+
+    
+
     pprint.pprint(vars(args), width=1000)
     with open(args.returnParameterFile, 'w') as f:
         f.write('>> parsed arguments\n')
