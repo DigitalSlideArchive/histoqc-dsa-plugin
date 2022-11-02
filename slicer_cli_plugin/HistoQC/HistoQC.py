36dcf44aea051fabfa384447255c34848ebee553
Robert Toth <robert.toth@thetatech.ai>
2022-11-02 21:13:51 +0000

    Integrated Andrew's base slicer_cli code into repo

---
+++ slicer_cli_plugin/HistoQC/HistoQC.py
@@ -0,0 +1,18 @@
+import pprint
+
+from slicer_cli_web import ctk_cli_adjustment  # noqa - imported for side effects
+from ctk_cli import CLIArgumentParser
+
+def main(args):
+    print('Entering main function.')
+    print('%r' % args)
+    pprint.pprint(vars(args), width=1000)
+    with open(args.returnParameterFile, 'w') as f:
+        f.write('>> parsed arguments\n')
+        f.write('%r\n' % args)
+    with open(args.arg1, 'w') as f:
+        f.write('example\n')
+
+
+if __name__ == '__main__':
+    main(CLIArgumentParser().parse_args())
