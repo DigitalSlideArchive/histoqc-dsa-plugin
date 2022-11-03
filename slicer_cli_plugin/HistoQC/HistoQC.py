4222d65603eb601d7d305900c0d66a845fff386e
Robert Toth <robert.toth@thetatech.ai>
2022-11-03 03:45:55 +0000

    Use girder client to get a folder and its items

--- slicer_cli_plugin/HistoQC/HistoQC.py
+++ slicer_cli_plugin/HistoQC/HistoQC.py
@@ -3,6 +3,8 @@ import pprint
 from slicer_cli_web import ctk_cli_adjustment  # noqa - imported for side effects
 from ctk_cli import CLIArgumentParser
 
+import girder_client
+
 import os
 
 import logging
@@ -15,26 +17,56 @@ histoqc_dir = '/opt/HistoQC' # from https://github.com/Theta-Tech-AI/histoqc-dsa
 histoqc_module_subdir = 'histoqc'
 
 
-def get_item(args):
-    image_path = os.path.abspath(args.input_image)
-    logging.info(f'image_path = {image_path}')
-    logging.info(f'image exists = {os.path.isfile(image_path)}')
+# def getHistoqcOutputFolder(user):
+#     return Folder().createFolder(
+#         parent = user,
+#         parentType='user',
+#         name = histoqc_output_folder_name,
+#         description='Folder to store the histoqc outputs for a user',
+#         public = False,
+#         reuseExisting = True) # reuseExisting will first search for an existing folder
 
-    subdir = os.path.basename(os.path.dirname(image_path))
-    item_id = subdir
-    logging.info(f'item_id = {item_id}')
-    return {'image_path': image_path, 'item_id': item_id}
 
+def get_folder(args, girder):
+
+    input_dir = args.inputDir
+    logging.info(f'input_dir = {input_dir}')
+
+
+    # by convention, it appears that the folder id is name of the parent directory - there is probably a better way to directly pass that in from the slicer cli
+    folder_id = os.path.basename(os.path.dirname(input_dir))
+    logging.info(f'folder_id = {folder_id}')
+
+    filenames = os.listdir(input_dir)
+    logging.info(f'filenames = {filenames}')
+
+    item_ids = {}
+    for filename in filenames:
+        item = next(girder.listItem(folder_id, name=filename, limit=1))
+        logging.info(f'item = {item}')
+        item_id = item['_id']
+        item_ids[filename] = item_id
+    
+    return {'input_dir': input_dir, 'item_ids': item_ids}
+
+
+def get_girder_client(args):
+    gc = girder_client.GirderClient(apiUrl=args.girderApiUrl)
+    gc.setToken(args.girderToken)
+    return gc
 
 
 def main(args):
     logging.info('Entering main function')
     logging.info('args = %r' % args)
 
-    item = get_item(args)
-    logging.info(f'item = {item}')
-
     logging.info(f'Files in {histoqc_dir} = {os.listdir(os.path.join(histoqc_dir,histoqc_module_subdir))}')
+
+    girder = get_girder_client(args)
+    
+    folder = get_folder(args, girder)
+    logging.info(f'folder = {folder}')
+
     
 
 if __name__ == '__main__':
