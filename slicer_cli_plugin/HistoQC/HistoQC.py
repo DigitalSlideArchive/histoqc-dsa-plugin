f9883be5d277180e5d808cd55481b42bed768ad1
Robert Toth <robert.toth@thetatech.ai>
2022-11-03 04:05:08 +0000

    HistoQC is run on all the images in a folder

--- slicer_cli_plugin/HistoQC/HistoQC.py
+++ slicer_cli_plugin/HistoQC/HistoQC.py
@@ -1,20 +1,15 @@
-import pprint
-
 from slicer_cli_web import ctk_cli_adjustment  # noqa - imported for side effects
 from ctk_cli import CLIArgumentParser
-
 import girder_client
 
 import os
-
+import tempfile
 import logging
+import subprocess
+
 logging.basicConfig(level=logging.INFO,
                     format='%(asctime)s.%(msecs)03d %(levelname)s %(module)s - %(funcName)s: %(message)s',
                     datefmt='%Y-%m-%d %H:%M:%S')
-
-# TODO: parameterize this so it knows where histoqc is located from the DockerFile
-histoqc_dir = '/opt/HistoQC' # from https://github.com/Theta-Tech-AI/histoqc-dsa-plugin/blob/13a82ab5bb42ca09104d887c1e6e6cff3a839ced/slicer_cli_plugin/Dockerfile#L24
-histoqc_module_subdir = 'histoqc'
 
 
 # def getHistoqcOutputFolder(user):
@@ -27,11 +22,31 @@ histoqc_module_subdir = 'histoqc'
 #         reuseExisting = True) # reuseExisting will first search for an existing folder
 
 
+def run_histoqc(input_folder):
+    # we will remember which directory we're currently in and only temporarily move into the histoqc directory for running it
+    # TODO: parameterize this so it knows where histoqc is located from the DockerFile
+    histoqc_dir = '/opt/HistoQC' # from https://github.com/Theta-Tech-AI/histoqc-dsa-plugin/blob/13a82ab5bb42ca09104d887c1e6e6cff3a839ced/slicer_cli_plugin/Dockerfile#L24
+    current_dir = os.getcwd()
+    input_dir = input_folder['input_dir']
+
+    with tempfile.TemporaryDirectory() as tmp_output_dir:
+    
+        logging.info('Running HistoQC on all files. Please wait, this may take a few minutes...')
+        os.chdir(histoqc_dir)
+        histoqc_output = subprocess.check_output(
+            ["python3", "-m", "histoqc",
+                '-o', tmp_output_dir,
+                f'{input_dir}/*'])
+        os.chdir(current_dir)
+        logging.info('HistoQC finished running.')
+
+        logging.info(f'Output files = {os.listdir(tmp_output_dir)}')
+
+
 def get_folder(args, girder):
 
     input_dir = args.inputDir
     logging.info(f'input_dir = {input_dir}')
-
 
     # by convention, it appears that the folder id is name of the parent directory - there is probably a better way to directly pass that in from the slicer cli
     folder_id = os.path.basename(os.path.dirname(input_dir))
@@ -60,13 +75,12 @@ def main(args):
     logging.info('Entering main function')
     logging.info('args = %r' % args)
 
-    logging.info(f'Files in {histoqc_dir} = {os.listdir(os.path.join(histoqc_dir,histoqc_module_subdir))}')
-
     girder = get_girder_client(args)
     
     folder = get_folder(args, girder)
     logging.info(f'folder = {folder}')
 
+    run_histoqc(folder)
     
 
 if __name__ == '__main__':
