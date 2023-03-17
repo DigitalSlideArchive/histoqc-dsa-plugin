36aa2a425945b309ed7f90a53cbfce758d70c6d5
Robert Toth <robert.toth@thetatech.ai>
2023-03-17 01:39:27 +0000

    Do not render histoqc html on the subfolder containing the outputs

--- histoqc/web_client/main.js
+++ histoqc/web_client/main.js
@@ -4,12 +4,19 @@ import {renderHistoQC} from './js/histoqc_view'
 
 
 wrap(HierarchyWidget, 'render', function (render) {
+
     render.call(this);
     console.log(this)
+    
     const is_folder = this.parentModel.attributes._modelType == "folder"
     if (!is_folder) return
+    const folder_name = this.parentModel.attributes.name
+    if (folder_name == 'histoqc_outputs') return
+
     const folder_id = this.parentModel.id
     console.log('folder_id = ', folder_id)
-    const widget =  this.$('.g-hierarchy-widget')
+    
+    const widget = this.$('.g-hierarchy-widget')
     renderHistoQC(widget, folder_id)
-});
+
+})
